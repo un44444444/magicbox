ebf08eb8cb765b3c2f54c7c352a9129b7f73e223
un44444444 <un44444444@gmail.com>
2013-09-03 03:14:35 +0800

    add QtWebView link navigation support

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,21 +8,38 @@ MainWindow::MainWindow(QWidget *parent)
 {
     //! The object we will expose to JavaScript engine:
     m_magicbox = new CMagicBox(this);
+
     // Signal is emitted before frame loads any web content:
     QObject::connect(page()->mainFrame(), SIGNAL(javaScriptWindowObjectCleared()),
                      this, SLOT(addJSObject()));
 
+    // Try to handle cicks by self
+    page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);//Handle link clicks by yourself
+    this->setContextMenuPolicy(Qt::NoContextMenu); //No context menu is allowed if you don't need it
+    QObject::connect(page(), SIGNAL( linkClicked( QUrl ) ),
+                  this, SLOT( linkClickedSlot( QUrl ) ) );
+
     // qrc:// URLs refer to resources. See views.qrc
     QUrl startURL = QUrl("qrc:/index.html");
     // Load web content now!
-    setUrl(startURL);
+    this->setUrl(startURL);
 }
 
 MainWindow::~MainWindow()
 {
 }
 
-void MainWindow::addJSObject() {
+void MainWindow::addJSObject()
+{
     // Add to JavaScript Frame as member "magicbox".
     page()->mainFrame()->addToJavaScriptWindowObject(QString("magicbox"), m_magicbox);
+}
+
+//This slot handles all clicks
+void MainWindow::linkClickedSlot( QUrl url )
+{
+    if (url.isValid())//isHtml does not exist actually you need to write something like it by yourself
+         this->load (url);
+    else//non html (pdf) pages will be opened with default application
+        QDesktopServices::openUrl( url );
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,7 @@ private:
 
 private slots:
     void addJSObject();
+    void linkClickedSlot( QUrl url );
 };
 
 #endif // MAINWINDOW_H
